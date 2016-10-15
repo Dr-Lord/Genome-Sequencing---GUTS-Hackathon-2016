2574dfce5b84648dc29993111717cc8fc8a43666
Dr-Lord <Lord.Fletcher@outlook.com>
2016-10-15 04:37:48 +0100

    Steps forward
    Settled on an algorithm. Started one implementation. Interrpted, started
    another. Unfinished, not working.

--- Sequencer.hs
+++ Sequencer.hs
@@ -24,6 +24,7 @@
 ---- 1 - IMPORTS AND TYPE DECLARATIONS -----------------------------------------
 
 import Data.List.Split as S (split, endsWith)
+import Data.List ((\\), union, delete)
 
 
 
@@ -57,9 +58,39 @@ main = do
     
     
     
-    
+   
+    -- Split partition-Y on tag-X then try and merge partitions X and Y
+--merge :: String -> [String] -> [String] -> [String]
+--merge xTag xPart yPart = 
     
 
+    -- Join a partitioned-by-tag-X partition-Y element with adjacent partition-Y elements
+    -- by looking for split partition-X elements on bondaries
+        -- NOTE: Ignore and redo at end instances where there is more than one possible split
+        -- partition-X element on a boundary (the set of remaining partition-X elements decreases every step)
+        -- NOTE: Cater for possibility of current incompletability (all possibility sets are greater than one for a full pass)
+        -- PERHAPS it would be useful to sort the yPartXed list by decreasing total original length of chunks
+joinAdjacents :: [[String]] -> [String] -> [String]
+joinAdjacents yPartXed []    = yPartXed
+joinAdjacents yPartXed xPart = joinAdjacents' yPartXed xPart [] $ length yPartXed
+    where joinAdjacents' []       xp res _ = joinAdjacents res xp
+          joinAdjacents' (e:ypxd) xp res n = joinAdjacents' ypxd' (xp \\ (middleVals e)) res' n'
+            | length lAdjs == 0 && length rAdjs == 0 = 
+            | 
+            where ypxd' = ypxd \\ (le `union` re)
+                  res'  = [[lAdj] ++ e ++ [rAdj]]:(delete e res)
+                  n' = length ypxd' + length res'
+                  lAdjs = filter () allChunkss
+                  rAdjs = filter () allChunkss
+                  (l,r) = (head e, last e)
+                  allChunkss = ypxd ++ res
+                
+                
+    
+    pool = xPart \\ middles
+    (extrema, middles) = map extsAndMids yPartXed
+        -- And then just combine the exts in order to get pool items
+          
 
 
 ---- 5 - OTHER FUNCTIONS -------------------------------------------------------
@@ -79,3 +110,19 @@ endsWith xs ys = beginsWith ws zs
     -- Splits the second string into chunks ending with the first string
 splitOn :: String -> String -> [String]
 splitOn xs = S.split (S.endsWith xs)
+
+
+    -- Returns all elements of a list except edge ones
+middleVals :: [a] -> [a]
+--middleVals [] = [] -- Should never happen
+middleVals [_] = []
+middleVals [_,_] = []
+middleVals (x:xs) = init xs
+
+
+    -- Separates edge and middle elements of a list
+extsAndMids :: [a] -> ([a],[a])
+--extsAndMids [] = ([],[]) -- Should never happen
+extsAndMids l@[x]   = (l,[])
+extsAndMids l@[x,y] = (l,[])
+extsAndMids (x:xs)  = ([x, last xs], init xs)
